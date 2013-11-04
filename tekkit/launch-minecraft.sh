76b1e8413ee89465189057e84322fafd0959e5c3
Matt Thompson <chameleonator@gmail.com>
2013-11-03 21:13:59 -0600

    Adding MCPC and bukkit plugins

--- tekkit/launch-minecraft.sh
+++ tekkit/launch-minecraft.sh
@@ -2,7 +2,8 @@
 
 cd /minecraft
 
+# make sure the server files are up to date
 rsync --verbose --ignore-existing --recursive /minecraft-source/ /minecraft/
 
 echo "Running minecraft server..."
-java -Xmx3G -Xms2G -jar Tekkit.jar nogui
+java -Xmx3G -Xms2G -jar MCPC.jar nogui
