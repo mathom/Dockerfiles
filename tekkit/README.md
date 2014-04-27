3ce0c59a7d23155271d47ff1f1082841038f302c
Matt Thompson <chameleonator@gmail.com>
2014-04-26 19:05:15 -0500

    Updating tekkit

---
+++ tekkit/README.md
@@ -0,0 +1,2 @@
+Example:
+docker run -d -t -i -p 25565:25565 -v /media/tekkit2:/minecraft tekkit
--- tekkit/launch-minecraft.sh
+++ tekkit/launch-minecraft.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+MCPC=MCPC.jar
+
 cd /minecraft
 
 # make sure the server files are up to date
 rsync --verbose --ignore-existing --recursive /minecraft-source/ /minecraft/
 
+chown -R minecraft:minecraft .
 echo "Running minecraft server..."
-java -Xmx3G -Xms2G -jar MCPC.jar nogui
+exec /sbin/setuser minecraft java -Xmx3G -Xms2G -jar $MCPC nogui
